5f0aabb592651f629d787dc15047a3274c349e7f
FelipeGomeees <felipegomes.dsn@gmail.com>
2026-03-01 15:47:54 -0300

    feat: barrel import core components should have commited in the previous branch

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -4,6 +4,7 @@ export { default as CoreSection } from './section/CoreSection.vue'
 export { default as CoreRadioButton } from './radiobutton/CoreRadioButton.vue'
 export { default as CoreRadioGroup } from './radiogroup/CoreRadioGroup.vue'
 export { default as CoreIconButton } from './iconbutton/CoreIconButton.vue'
-export { default as CoreCard } from './card/CoreCard.vue'
-export { default as CoreDivider } from './divider/CoreDivider.vue'
 export { default as CoreSurfaceStack } from './surfacestack/CoreSurfaceStack.vue';
+export { default as CoreSkeleton } from './skeleton/CoreSkeleton.vue';
+export { default as CoreOverlay } from './overlay/CoreOverlay.vue';
+export { default as CoreDialog } from './dialog/CoreDialog.vue';
