2ec507b1f73ec51d9c221fd41593559374a8f5f6
FelipeGomeees <felipegomes.dsn@gmail.com>
2026-03-01 15:50:05 -0300

    feat: router transition trigger should have send in previous commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,21 +4,31 @@ import ExtratoView from '@/views/extrato/ExtratoView.vue'
 import MovimentacaoView from '@/views/movimentacao/MovimentacaoView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            component: HomeView,
-        },
-        {
-            path: '/extrato',
-            component: ExtratoView,
-        },
-        {
-            path: '/movimentacao',
-            component: MovimentacaoView,
-        }
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: HomeView,
+    },
+    {
+      path: '/extrato',
+      component: ExtratoView,
+    },
+    {
+      path: '/extrato/movimentacao',
+      component: MovimentacaoView,
+    }
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  const toDepth = to.path.split('/').length
+  const fromDepth = from.path.split('/').length
+
+  to.meta.transition =
+    toDepth > fromDepth ? 'pop-forward' : 'pop-back'
+
+  next()
 })
 
 export default router
