fcd58e8174518c47760f70d19ef5debb02d84d4d
FelipeGomeees <felipegomes.dsn@gmail.com>
2026-03-01 15:46:03 -0300

    feat: app transition

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const transitionName = computed(() => {
+  return route.meta.transition || 'pop-forward'
+})
 </script>
 
 <template>
-  <main>
-    <RouterView/>
-  </main>
+    <Transition :name="transitionName" mode="out-in">
+      <div class="route-viewport" :key="route.fullPath">
+        <RouterView></RouterView>
+      </div>
+    </Transition>
 </template>
 
 <style>
-  * {
-    font-family: "SN Pro", sans-serif;
-  }
+    * {
+      font-family: "SN Pro", sans-serif;
+    }
+    .route-viewport {
+      position: fixed;
+      inset: 0;
+      overflow-y: auto;
+      background: var(--color-background);
+    }
+    .pop-forward-enter-active,
+    .pop-forward-leave-active {
+      transition: all 0.2s ease;
+    }
+
+    .pop-forward-enter-from {
+      opacity: 0;
+      transform: scale(0.97);
+    }
+
+    .pop-forward-leave-to {
+      opacity: 0;
+      transform: scale(1.03);
+    }
+    
+
+    .pop-back-enter-active,
+    .pop-back-leave-active {
+      transition: all 0.2s ease;
+    }
+
+    .pop-back-enter-from {
+      opacity: 0;
+      transform: scale(1.03);
+    }
+
+    .pop-back-leave-to {
+      opacity: 0;
+      transform: scale(0.97);
+    }
 </style>
