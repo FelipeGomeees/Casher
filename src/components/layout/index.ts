e867020d11022db5813a40a7e125772b650da681
FelipeGomeees <felipegomes.dsn@gmail.com>
2026-03-06 11:34:13 -0300

    feat: barrel imports

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,5 @@
 export { default as LayoutPage } from './page/LayoutPage.vue';
 export { default as LayoutHeader } from './header/LayoutHeader.vue';
 export { default as LayoutHeaderAction } from './header/LayoutHeaderAction.vue';
+export { default as LayoutFormSection } from './formsection/LayoutFormSection.vue';
+export { default as LayoutFormSectionGrid } from './formsectiongrid/LayoutFormSectionGrid.vue';
